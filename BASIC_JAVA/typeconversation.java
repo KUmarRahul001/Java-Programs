87fdd3afaabfa40468099358746f5828161a91bf
KUmarRahul001 <kumarrahulraj468@outlook.com>
2024-06-18 18:03:44 +0530

    Basic Java Codes

---
+++ BASIC_JAVA/typeconversation.java
@@ -0,0 +1,13 @@
+public class typeconversation {
+
+    public static void main(String[] args) {
+        int a = 33;
+        int b = 22;
+        if (a == b) {
+            System.err.println("True");
+        }
+        else{
+            System.out.println("No");
+        }
+    }
+}
