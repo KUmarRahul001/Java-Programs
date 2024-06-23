0c93a7df4d3e3dd75fe3e376be4c6a54305a7de5
KUmarRahul001 <kumarrahulraj468@outlook.com>
2024-06-24 01:33:48 +0530

    Comparsion Opearotr added in Opearator Folder

---
+++ BASIC_JAVA/opearators/Comparsionop.java
@@ -0,0 +1,29 @@
+package Opearators;
+
+/**
+ * Comparsionop
+ */
+public class Comparsionop {
+
+    public static void main(String[] args) {
+        int a = 10, b = 20, c = 10, d = 0;
+        System.out.println(a == b);
+        System.out.println(a!= b);
+        System.out.println(a > b);
+        System.out.println(a < b);
+        System.out.println(a >= b);
+        System.out.println(a <= b);
+        System.out.println(a == c);
+        System.out.println(a!= c);
+        System.out.println(a > c);
+        System.out.println(a < c);
+        System.out.println(a >= c);
+        System.out.println(a <= c);
+        System.out.println(a == d);
+        System.out.println(a!= d);
+        System.out.println(a > d);
+        System.out.println(a < d);
+        System.out.println(a >= d);
+        System.out.println(a <= d);
+        }
+}
